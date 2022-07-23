6c98ffe89df225da69c26abc11b966855994be1a
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-07-23 16:54:21 +0600

    error solve in create room and fetch romms

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,18 +1,20 @@
 import Room from '../models/roomSchema.js'
 
-const allRooms = (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: 'All rooms'
-    })
+const allRooms = async (req, res) => {
+    try{
+        const rooms = await Room.find()
+        res.status(200).json({ success: true, rooms, count: rooms.length, message: 'Rooms loaded successfully!' })
+    }catch(error){
+        res.status(400).json({ success: false, message: 'Room loaded failed!', error: error.message })
+    }
 }
 
-const createRoom = (req, res) => {
+const createRoom = async (req, res) => {
     try{
         const room = await Room.create(req.body)
-        res.status(200).json({ success: true, message: 'Room created successfully!' })
+        res.status(200).json({ success: true, room, message: 'Room created successfully!' })
     }catch(error){
-        res.status(200).json({ success: false, message: 'Room created failed!', error: error.message })
+        res.status(400).json({ success: false, message: 'Room created failed!', error: error.message })
     }
 }
 
